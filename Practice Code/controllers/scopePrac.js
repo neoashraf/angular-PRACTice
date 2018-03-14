2b528bb8b9059eadfb0ed78dbe23099e592cea70
_neo_ <ashrafuddin160@gmail.com>
2018-03-15 03:47:10 +0600

    fixed controller error of previous commit

--- Practice Code/controllers/scopePrac.js
+++ Practice Code/controllers/scopePrac.js
@@ -1,10 +1,10 @@
-console.log("1");
-function scopePractice(){
-    this.sortBy = 'name';
-    this.reverse= false;
-    this.Employees=[ {joined:'2000-12-02', name:'Ashraf', city:'Dhaka', salary:'30000'},{joined:'2013-12-02', name:'Xshraf', city:'Bhaka', salary:'34000'},{joined:'2005-12-02', name:'Pshraf', city:'Xhaka', salary:'60000'}];
-    this.doSort=function (propName){
-        this.sortBy = propName;
-        this.reverse= !this.reverse;
+var app = angular.module('myApp',[]);
+app.controller( 'scopePractice',function ($scope) {
+    $scope.sortBy = 'name';
+    $scope.reverse= false;
+    $scope.Employees=[ {joined:'2000-12-02', name:'Ashraf', city:'Dhaka', salary:'30000'},{joined:'2013-12-02', name:'Xshraf', city:'Bhaka', salary:'34000'},{joined:'2005-12-02', name:'Pshraf', city:'Xhaka', salary:'60000'}];
+    $scope.doSort=function (propName){
+        $scope.sortBy = propName;
+        $scope.reverse=!$scope.reverse;
     };
-}
+});
