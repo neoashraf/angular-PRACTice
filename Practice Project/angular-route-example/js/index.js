099046f7bd3310a8543119226fe4a36462b1e544
_neo_ <ashrafuddin160@gmail.com>
2018-03-18 04:26:43 +0600

    Edited

--- Practice Project/angular-route-example/js/index.js
+++ Practice Project/angular-route-example/js/index.js
@@ -17,8 +17,7 @@ angular.module('ngView', [], function($routeProvider, $locationProvider) {
   // configure html5 to get links working on jsfiddle
   $locationProvider.html5Mode(true);
 });
-
-
+ 
 function MainCntl($scope, $route, $routeParams, $location) {
   $scope.$route = $route;
   $scope.$location = $location;
