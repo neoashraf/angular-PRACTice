478fe1113124c53cfc8740230d191f195076144e
_neo_ <ashrafuddin160@gmail.com>
2018-03-20 03:06:40 +0600

    checkbox-prac

--- Practice Code/controllers/scopePrac.js
+++ Practice Code/controllers/scopePrac.js
@@ -1,10 +1,25 @@
-var app = angular.module('myApp',[]);
-app.controller( 'scopePractice',function ($scope) {
+var app = angular.module('myApp', []);
+app.controller('scopePractice', function ($scope) {
     $scope.sortBy = 'name';
-    $scope.reverse= false;
-    $scope.Employees=[ {joined:'2000-12-02', name:'Ashraf', city:'Dhaka', salary:'30000'},{joined:'2013-12-02', name:'Xshraf', city:'Bhaka', salary:'34000'},{joined:'2005-12-02', name:'Pshraf', city:'Xhaka', salary:'60000'}];
-    $scope.doSort=function (propName){
+    $scope.reverse = false;
+    $scope.Employees = [{
+        joined: '2000-12-02',
+        name: 'Ashraf',
+        city: 'Dhaka',
+        salary: '30000'
+    }, {
+        joined: '2013-12-02',
+        name: 'Xshraf',
+        city: 'Bhaka',
+        salary: '34000'
+    }, {
+        joined: '2005-12-02',
+        name: 'Pshraf',
+        city: 'Xhaka',
+        salary: '60000'
+    }];
+    $scope.doSort = function (propName) {
         $scope.sortBy = propName;
-        $scope.reverse=!$scope.reverse;
+        $scope.reverse = !$scope.reverse;
     };
 });
